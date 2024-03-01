e348e2926fe10966123f43ace81ed8fed2cd6e0e
Luis Masuelli <luisfmasuelli@gmail.com>
2024-03-01 08:35:05 -0500

    Adding some settings to the script.

--- sample-network.sh
+++ sample-network.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ganache-cli --database.dbPath=./luis.ganache.db --mnemonic 'concert ladder decline crumble mention because pepper address scan volco pulse pig'
+ganache-cli --database.dbPath=./luis.ganache.db --mnemonic 'concert ladder decline crumble mention because pepper address scan volco pulse pig' --wallet.defaultBalance 1000 --wallet.totalAccounts 100
